d5563d2d258cd9b1c783b6dbcb443f6796cb6dad
Tyler Dean <Tyler_Dean1@baylor.edu>
2023-08-29 16:38:34 -0500

    initial commit

--- script1.py
+++ script1.py
@@ -4,13 +4,13 @@
 
 def calcTotal(amt,tax,name):
     mytotal = amt * (1+tax)
-    print(f"{myname}, the total of your purchase is ${total:,.2f}")
+    print(f"{myname}, the total of your purchase is ${mytotal:,.2f}")
 
 myname = 'Johnny'
-price = 1,560
-thetax = 8.25%
+price = float(1560)
+thetax = float(.0825)
 
-calcTotal(thetax,price,name)
+calcTotal(price, thetax, myname)
 
 
 
@@ -21,7 +21,8 @@ calcTotal(thetax,price,name)
 list1 = [1,'two',3.2,'four',5]
 
 for i in list1:
-    print(i)
+    if type(i) == int:
+        print(i)
 
 
 
